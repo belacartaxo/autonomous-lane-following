a5680a0d8457b850f04e9ac63c3963ccef16044c
Rodrigo Simões <rodrigo.simoes0504@gmail.com>
2026-06-18 15:08:03 +0100

    Add files via upload

--- configs/evaluation_config.py
+++ configs/evaluation_config.py
@@ -3,7 +3,7 @@ RESULTS_DIR = "./results"
 SEED = 42
 N_EPISODES = 100
 NOISE_STD = 0.1
-DROPOUT_PROB = 0.1   
+DROPOUT_PROB = 0.1
 
 SCENARIOS = {
     "c1": {
@@ -11,6 +11,7 @@ SCENARIOS = {
         "env_type": "base",
         "use_noise": False,
         "has_dynamic_obstacles": False,
+        "result_prefix": "c1",
         "models": {
             "ppo": f"{MODELS_DIR}/c1_ppo_default_best.zip",
             "dqn": f"{MODELS_DIR}/c1_dqn_default_best.zip",
@@ -22,6 +23,7 @@ SCENARIOS = {
         "env_type": "base",
         "use_noise": True,
         "has_dynamic_obstacles": False,
+        "result_prefix": "c2",
         "models": {
             "ppo": f"{MODELS_DIR}/c2_ppo_default_noise_best.zip",
             "dqn": f"{MODELS_DIR}/c2_dqn_default_noise_best.zip",
@@ -33,6 +35,7 @@ SCENARIOS = {
         "env_type": "base",
         "use_noise": False,
         "has_dynamic_obstacles": False,
+        "result_prefix": "c3",
         "models": {
             "ppo": f"{MODELS_DIR}/c3_ppo_static_best.zip",
             "dqn": f"{MODELS_DIR}/c3_dqn_static_best.zip",
@@ -44,6 +47,7 @@ SCENARIOS = {
         "env_type": "base",
         "use_noise": True,
         "has_dynamic_obstacles": False,
+        "result_prefix": "c4",
         "models": {
             "ppo": f"{MODELS_DIR}/c4_ppo_static_noise_best.zip",
             "dqn": f"{MODELS_DIR}/c4_dqn_static_noise_best.zip",
@@ -55,6 +59,7 @@ SCENARIOS = {
         "env_type": "critical",
         "use_noise": False,
         "has_dynamic_obstacles": True,
+        "result_prefix": "c5",
         "models": {
             "ppo": f"models/ppo_dynamic/ppo_dynamic_best/best_model.zip",
             "dqn": f"{MODELS_DIR}/c5_dqn_dynamic_best.zip",
@@ -66,6 +71,7 @@ SCENARIOS = {
         "env_type": "critical",
         "use_noise": True,
         "has_dynamic_obstacles": True,
+        "result_prefix": "c6",
         "models": {
             "ppo": f"{MODELS_DIR}/c6_ppo_dynamic_noise_best.zip",
             "dqn": f"{MODELS_DIR}/c6_dqn_dynamic_noise_best.zip",
