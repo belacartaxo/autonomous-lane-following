c5337db554820370dba69043c10073657923d7a2
beatrizseabra <bea.seabra@gmail.com>
2026-06-18 18:41:27 +0100

    Refactor Webots path setup into separate function

--- ppo/train_ppo.py
+++ ppo/train_ppo.py
@@ -1,25 +1,7 @@
 import os
-import sys
-import platform
 
-if platform.system() == "Windows":
-    WEBOTS_HOME = r"C:\Program Files\Webots"
-elif platform.system() == "Darwin":  # macOS
-    WEBOTS_HOME = "/Applications/Webots.app"
-else:  # Linux
-    WEBOTS_HOME = "/usr/local/webots"
-
-os.environ["WEBOTS_HOME"] = WEBOTS_HOME
-
-WEBOTS_PYTHON_PATH = os.path.join(
-    WEBOTS_HOME,
-    "lib",
-    "controller",
-    "python"
-)
-
-if WEBOTS_PYTHON_PATH not in sys.path:
-    sys.path.insert(0, WEBOTS_PYTHON_PATH)
+from webots_setup import setup_webots_path
+setup_webots_path()
     
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback, CallbackList
